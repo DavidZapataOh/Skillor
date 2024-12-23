b9fbb9005d3a6cdb6fb04f03916094e8a844e019
DavidZapataOh <davidzapata5885@gmail.com>
2024-12-22 21:55:45 -0500

    Add Landing Page

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -10,7 +10,8 @@ export default {
       colors: {
         primary: '#7FB800',
         secondary: '#466600',
-        background: '#2F2F2F',
+        background: '#141414',
+        background_secondary: '#0A0A0A',
         card: '#1F2937',
         text: '#FCF7FF',
         error: '#FF4D4F',
