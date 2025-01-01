30af64ecbfd0707f4527021c6c7d74a69fa4079b
David Zapata <davidzapata5885@gmail.com>
2025-01-01 15:41:03 -0500

    deploy erc20 token on fuji network

--- hardhat/contracts/SKLLR.sol
+++ hardhat/contracts/SKLLR.sol
@@ -7,11 +7,16 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract SKLLR is ERC20, Ownable {
     constructor(address initialOwner)
-        ERC20("SkillerReputationToken", "SKLLR")
+        ERC20("SkillorToken", "SKLLR")
         Ownable(initialOwner)
     {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
+    }
+
+    function _update(address from, address to, uint256 amount) internal override {
+        require(from == address(0) || to == address(0), "Tokens are non-transferable");
+        super._update(from, to, amount);
     }
 }
--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -3,13 +3,18 @@ require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  defaultNetwork: "sepolia",
+  defaultNetwork: "fuji",
   networks: {
     hardhat: {
     },
     sepolia: {
       url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: [process.env.PRIVATE_KEY]
+    },
+    fuji: {
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 43113
     }
   },
   solidity: {
